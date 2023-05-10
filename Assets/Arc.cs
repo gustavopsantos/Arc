322c6cee461c40a45a12bf92ad96a0d83ae8efd7
Gustavo Santos <gustappsantos@gmail.com>
2023-05-10 12:09:57 -0300

    Split arc definition from its behaviours

--- Assets/Arc.cs
+++ Assets/Arc.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -12,78 +11,13 @@ public readonly struct Arc
 
     public Arc(Vector3 start, Vector3 normal, float sweepAngle)
     {
+        Assert.IsTrue(sweepAngle >= 0, "Sweep angle must be positive");
+        
         Start = start;
         Normal = normal;
         SweepAngle = sweepAngle;
 
         Middle = Quaternion.AngleAxis(sweepAngle / 2, normal) * start;
         End = Quaternion.AngleAxis(sweepAngle, normal) * start;
-    }
-
-    public Arc Rotate(float angle)
-    {
-        return new Arc(Quaternion.AngleAxis(angle, Normal) * Start, Normal, SweepAngle);
-    }
-
-    public Arc Resize(float degrees)
-    {
-        var from = Quaternion.AngleAxis(-degrees / 2f, Normal) * Start;
-        var sweepAngle = SweepAngle + degrees;
-        return new Arc(from, Normal, sweepAngle);
-    }
-
-    public Arc Clamp(Arc outer)
-    {
-        return Clamp(this, outer);
-    }
-
-    public static Arc Clamp(Arc inner, Arc outer)
-    {
-        Assert.IsTrue(inner.Normal == outer.Normal, "Arcs must have the same normal");
-
-        if (inner.SweepAngle > outer.SweepAngle)
-        {
-            return outer;
-        }
-
-        if (TryGetOvershoot(inner, outer, inner.Normal, out var overshoot))
-        {
-            return inner.Rotate(-overshoot);
-        }
-
-        return inner;
-    }
-
-    private static bool TryGetOvershoot(Arc inner, Arc outer, Vector3 normal, out float overshoot)
-    {
-        var outerTangent = Vector3.Cross(outer.Middle, -normal);
-        var innerSideLocalToOuter = Vector3.Dot(inner.Middle, outerTangent);
-
-        if (innerSideLocalToOuter >= 0) // Inner its on outer right quadrant
-        {
-            overshoot = -Vector3.SignedAngle(inner.End, outer.End, normal);
-
-            if (overshoot > 0)
-            {
-                return true;
-            }
-        }
-        else // Inner its on outer left quadrant
-        {
-            overshoot = -Vector3.SignedAngle(inner.Start, outer.Start, normal);
-
-            if (overshoot < 0)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void Draw(Color color)
-    {
-        Handles.color = color;
-        Handles.DrawSolidArc(Vector3.zero, Normal, Start, SweepAngle, 1);
     }
 }
---
+++ Assets/ArcExtensions.cs
@@ -0,0 +1,68 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public static class ArcExtensions
+{
+    public static Arc Rotate(this Arc arc, float angle)
+    {
+        return new Arc(Quaternion.AngleAxis(angle, arc.Normal) * arc.Start, arc.Normal, arc.SweepAngle);
+    }
+
+    public static Arc Resize(this Arc arc, float degrees)
+    {
+        var from = Quaternion.AngleAxis(-degrees / 2f, arc.Normal) * arc.Start;
+        var sweepAngle = arc.SweepAngle + degrees;
+        return new Arc(from, arc.Normal, sweepAngle);
+    }
+
+    public static void Draw(this Arc arc, Color color)
+    {
+        Handles.color = color;
+        Handles.DrawSolidArc(Vector3.zero, arc.Normal, arc.Start, arc.SweepAngle, 1);
+    }
+    
+    public static Arc Clamp(this Arc inner, Arc outer)
+    {
+        Assert.IsTrue(inner.Normal == outer.Normal, "Arcs must have the same normal");
+
+        if (inner.SweepAngle > outer.SweepAngle)
+        {
+            return outer;
+        }
+
+        if (TryGetOvershoot(inner, outer, inner.Normal, out var overshoot))
+        {
+            return inner.Rotate(-overshoot);
+        }
+
+        return inner;
+    }
+    
+    private static bool TryGetOvershoot(Arc inner, Arc outer, Vector3 normal, out float overshoot)
+    {
+        var outerTangent = Vector3.Cross(outer.Middle, -normal);
+        var innerSideLocalToOuter = Vector3.Dot(inner.Middle, outerTangent);
+
+        if (innerSideLocalToOuter >= 0) // Inner its on outer right quadrant
+        {
+            overshoot = -Vector3.SignedAngle(inner.End, outer.End, normal);
+
+            if (overshoot > 0)
+            {
+                return true;
+            }
+        }
+        else // Inner its on outer left quadrant
+        {
+            overshoot = -Vector3.SignedAngle(inner.Start, outer.Start, normal);
+
+            if (overshoot < 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
